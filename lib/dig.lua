c77b524f503d0fdb8f68f8bef0ad0f91466223ed
vita69 <vita.grafeisen.dev@gmail.com>
2017-01-24 19:37:08 +0900

    modify dig wrapper

--- lib/dig.lua
+++ lib/dig.lua
@@ -1,4 +1,6 @@
+dofile("./turn.lua")
 -- 指定方向のの障害物がなくなるまで掘る(又は攻撃する)
+-- direction=forward, back, up, down, left, right
 function dig(direction)
     local direction = direction or "forward"
     print("dig "..direction)
@@ -8,6 +10,13 @@ function dig(direction)
             turtle.dig()
             turtle.attack() -- mob用
         end
+    elseif direction == "back" then
+        turn("right", 2)
+        while turtle.detect() do
+            turtle.dig()
+            turtle.attack() -- mob用
+        end
+        turn("left", 2)
     elseif direction == "up" then
         while turtle.detect() do
             turtle.dig()
@@ -18,6 +27,20 @@ function dig(direction)
             turtle.dig()
             turtle.attack() -- mob用
         end
+    elseif direction == "left" then
+        turn("left")
+        while turtle.detect() do
+            turtle.dig()
+            turtle.attack() -- mob用
+        end
+        turn("right")
+    elseif direction == "right" then
+        turn("right")
+        while turtle.detect() do
+            turtle.dig()
+            turtle.attack() -- mob用
+        end
+        turn("left")
     end
     return true
 end
